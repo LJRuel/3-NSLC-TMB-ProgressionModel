653ebeba7e8380fab9fca1576b092403aab109ec
LJRuel <louis-jacques.ruel.1@ulaval.ca>
2023-01-19 17:02:40 -0500

    SBS profile

--- 1-92_NSLC_vcf_db.r
+++ 1-92_NSLC_vcf_db.r
@@ -1,6 +1,6 @@
 if (!require("BiocManager", quietly = TRUE))
   install.packages("BiocManager")
-BiocManager::install("GenomicRanges", force = TRUE)
+BiocManager::install("GenomicRanges")
 BiocManager::install("VariantAnnotation")
 #BiocManager::install("TxDb.Hsapiens.UCSC.hg19.knownGene")
 #BiocManager::install("BSgenome.Hsapiens.UCSC.hg19")
@@ -16,25 +16,44 @@ library(VariantAnnotation)
 library(stringr)
 library(tidyr)
 library(dplyr)
-
+library(glue)
 
 
 # Reference : https://bioconductor.org/packages/devel/bioc/vignettes/VariantAnnotation/inst/doc/VariantAnnotation.pdf
 
+Patient <- "0001"
+
 # Linux
-# VEP.first_line <- grep('##', readLines("/mnt/sda2/TMB/Data/VEP_92_NSLC/NSLC_0001.vcf"), invert = TRUE, fixed = TRUE, )[1]
-# VCF.info_lines <- grep('##INFO=', readLines("/mnt/sda2/TMB/Data/VEP_92_NSLC/NSLC_0001.vcf"), fixed = TRUE)
-# VEP.format_line <- last(VCF.info_lines)
-# VCF.info <- readLines("/mnt/sda2/TMB/Data/VEP_92_NSLC/NSLC_0001.vcf")[c(VCF.info_lines[1:length(VCF.info_lines)-1])] %>% gsub(".*ID=", "", .) %>% gsub(",.*", "", .)
-# VEP.format <- readLines("/mnt/sda2/TMB/Data/VEP_92_NSLC/NSLC_0001.vcf")[VEP.format_line] %>% gsub(".*Format: ", "", .) %>% gsub("\">", "", .) %>% str_split(., "\\|") %>% unlist()
-# VEP_data <- read.delim("/mnt/sda2/TMB/Data/VEP_92_NSLC/NSLC_0001.vcf", sep = "\t", skip = VEP.first_line-1, header = TRUE)
+if(Sys.info()['sysname'] == "Linux") {
+  # Clinicopathological data - keeping only adenocarinomas
+  clin_data <- read.xlsx("/mnt/sda2/TMB/Data/VEP_92_NSLC//NCI_NeverSmoker_n92_20210812_TMB_drivers.xlsx", sheetIndex = 1)
+  adeno_list <- clin_data$Patient_ID[clin_data$histology == "3"]
+  
+  
+  VEP.first_line <- grep('##', readLines(glue("/mnt/sda2/TMB/Data/VEP_92_NSLC/NSLC_{Patient}.vcf")), invert = TRUE, fixed = TRUE, )[1]
+  VCF.info_lines <- grep('##INFO=', readLines(glue("/mnt/sda2/TMB/Data/VEP_92_NSLC/NSLC_{Patient}.vcf")), fixed = TRUE)
+  VEP.format_line <- last(VCF.info_lines)
+  VCF.info <- readLines(glue("/mnt/sda2/TMB/Data/VEP_92_NSLC/NSLC_{Patient}.vcf"))[c(VCF.info_lines[1:length(VCF.info_lines)-1])] %>% gsub(".*ID=", "", .) %>% gsub(",.*", "", .)
+  VEP.format <- readLines(glue("/mnt/sda2/TMB/Data/VEP_92_NSLC/NSLC_{Patient}.vcf"))[VEP.format_line] %>% gsub(".*Format: ", "", .) %>% gsub("\">", "", .) %>% str_split(., "\\|") %>% unlist()
+  VEP_data <- read.delim(glue("/mnt/sda2/TMB/Data/VEP_92_NSLC/NSLC_{Patient}.vcf"), sep = "\t", skip = VEP.first_line-1, header = TRUE)
+  VEP_data <- VEP_data[VEP_data$X.CHROM != "MT",]
+}
+  
 # Windows
-VEP.header <- grep('##', readLines("Data/NSLC_0001.vcf"), invert = TRUE, fixed = TRUE, )[1]
-VCF.info_lines <- grep('##INFO=', readLines("Data/NSLC_0001.vcf"), fixed = TRUE)
-VEP.format_line <- last(VCF.info_lines)
-VCF.info <- readLines("Data/NSLC_0001.vcf")[c(VCF.info_lines[1:length(VCF.info_lines)-1])] %>% gsub(".*ID=", "", .) %>% gsub(",.*", "", .)
-VEP.format <- readLines("Data/NSLC_0001.vcf")[VEP.format_line] %>% gsub(".*Format: ", "", .) %>% gsub("\">", "", .) %>% str_split(., "\\|") %>% unlist()
-VEP_data <- read.delim("Data/NSLC_0001.vcf", sep = "\t", skip = VEP.first_line-1, header = TRUE)
+if(Sys.info()['sysname'] == "Windows") {
+  # Clinicopathological data - keeping only adenocarinomas
+  clin_data <- read.xlsx("Data/NCI_NeverSmoker_n92_20210812_TMB_drivers.xlsx", sheetIndex = 1)
+  adeno_list <- clin_data$Patient_ID[clin_data$histology == "3"]
+  
+  
+  VEP.header <- grep('##', readLines(glue("Data/VEP_92_NSLC/NSLC_{Patient}.vcf")), invert = TRUE, fixed = TRUE, )[1]
+  VCF.info_lines <- grep('##INFO=', readLines(glue("Data/VEP_92_NSLC/NSLC_{Patient}.vcf")), fixed = TRUE)
+  VEP.format_line <- last(VCF.info_lines)
+  VCF.info <- readLines(glue("Data/VEP_92_NSLC/NSLC_{Patient}.vcf"))[c(VCF.info_lines[1:length(VCF.info_lines)-1])] %>% gsub(".*ID=", "", .) %>% gsub(",.*", "", .)
+  VEP.format <- readLines(glue("Data/VEP_92_NSLC/NSLC_{Patient}.vcf"))[VEP.format_line] %>% gsub(".*Format: ", "", .) %>% gsub("\">", "", .) %>% str_split(., "\\|") %>% unlist()
+  VEP_data <- read.delim(glue("Data/VEP_92_NSLC/NSLC_{Patient}.vcf", sep = "\t", skip = VEP.header-1, header = TRUE))
+
+}
 
 # Data formatting
 VEP.extra <- strsplit(VEP_data$INFO, ";\\s*(?=[^;]+$)", perl=TRUE)
@@ -43,17 +62,7 @@ VEP_data$EXTRA <- sapply(VEP.extra, "[[", 2)
 VEP_data$EXTRA <- lapply(VEP_data$EXTRA, gsub, pattern = 'CSQ=', replacement='')
 VEP_data.EXTRA_split_rows <- separate_rows(VEP_data, EXTRA, sep=",")
 VEP_data.EXTRA_split_cols <- separate(VEP_data.EXTRA_split_rows, EXTRA, c(VEP.format), sep = "\\|")
-
-
-VCF <- keepSeqlevels(VCF, c(1:22, "X", "Y"), pruning.mode = "coarse")
-seqlevels(VCF) <- paste0("chr", c(1:22, "X", "Y"))
-VCF.range <- rowRanges(VCF)
-
-all.variants <- locateVariants(VCF.range, txdb, AllVariants())
-
-which(all.variants == trim(all.variants))
-which(end(all.variants) > seqlengths(BSgenome)[as.character(seqnames(all.variants))])
-all.variants[all.variants$QUERYID == 153]
+assign(glue("VEP_data.NSLC_{Patient}"), VEP_data.EXTRA_split_cols)
 
 
 
