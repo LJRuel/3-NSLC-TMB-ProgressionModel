ea238ad0db3d6c0ae8ba571d372aae45cc36f555
LJRuel <louis-jacques.ruel.1@ulaval.ca>
2023-01-23 16:31:34 -0500

    Update 1-92_NSLC_vcf_db.r

--- 1-92_NSLC_vcf_db.r
+++ 1-92_NSLC_vcf_db.r
@@ -10,33 +10,40 @@ if (!require("BiocManager", quietly = TRUE))
 #txdb <- TxDb.Hsapiens.UCSC.hg19.knownGene
 #BSgenome <- BSgenome.Hsapiens.UCSC.hg19
 
-library(readr)
+library(tidyverse)
 library(GenomicRanges)
 #library(VariantAnnotation)
-library(stringr)
-library(tidyr)
-library(dplyr)
-library(xlsx)
-library(glue)
-
-# Reference : https://bioconductor.org/packages/devel/bioc/vignettes/VariantAnnotation/inst/doc/VariantAnnotation.pdf
+#library(xlsx)
 
 Patient <- "0001"
+
+# Mutation types annotated by VEP are available here: https://grch37.ensembl.org/info/genome/variation/prediction/predicted_data.html
+region_types <- list()
+region_types[["intergenic"]] <- c("downstream_gene_variant", "upstream_gene_variant", "intergenic_variant")
+region_types[["intron"]] <- c("intron_variant", "non_coding_transcript_variant")
+region_types[["regulatory"]] <- c("regulatory_region_variant", "TF_binding_site_variant")
+region_types[["exon"]] <- c("non_coding_transcript_exon_variant", "missense_variant", "synonymous_variant", "3_prime_UTR_variant", "5_prime_UTR_variant", "coding_sequence_variant", "frameshift_variant", "stop_gained")
+region_types[["splice"]] <- c("splice_region_variant", "splice_donor_variant", "splice_acceptor_variant", "splice_polypyrimidine_tract_variant", "splice_donor_region_variant")
+
+region_types
 
 # Linux
 if(Sys.info()['sysname'] == "Linux") {
   # Clinicopathological data - keeping only adenocarinomas
-  clin_data <- read.xlsx("/mnt/sda2/TMB/Data/VEP_92_NSLC//NCI_NeverSmoker_n92_20210812_TMB_drivers.xlsx", sheetIndex = 1)
+  clin_data <- read.xlsx("/home/l_ruelou01/NSLC_TMB-Model/Data/NCI_NeverSmoker_n92_20210812_TMB_drivers.xlsx", sheetIndex = 1)
   adeno_list <- clin_data$Patient_ID[clin_data$histology == "3"]
   
-  VEP.header <- grep('##', readLines(glue("/mnt/sda2/TMB/Data/VEP_92_NSLC/NSLC_{Patient}.vcf")), invert = TRUE, fixed = TRUE, )[1]
-  VCF.info_lines <- grep('##INFO=', readLines(glue("/mnt/sda2/TMB/Data/VEP_92_NSLC/NSLC_{Patient}.vcf")), fixed = TRUE)
+  VEP.raw <- readLines(glue("/mnt/sda2/TMB/Data/VEP_pick_92_NSLC/NSLC_{Patient}.vcf"))
+  VEP.header <- grep('##', VEP.raw, invert = TRUE, fixed = TRUE, )[1]
+  VCF.info_lines <- grep('##INFO=', VEP.raw, fixed = TRUE)
   VEP.format_line <- last(VCF.info_lines)
-  VCF.info <- readLines(glue("/mnt/sda2/TMB/Data/VEP_92_NSLC/NSLC_{Patient}.vcf"))[c(VCF.info_lines[1:length(VCF.info_lines)-1])] %>% gsub(".*ID=", "", .) %>% gsub(",.*", "", .)
-  VEP.format <- readLines(glue("/mnt/sda2/TMB/Data/VEP_92_NSLC/NSLC_{Patient}.vcf"))[VEP.format_line] %>% gsub(".*Format: ", "", .) %>% gsub("\">", "", .) %>% str_split(., "\\|") %>% unlist()
-  VEP_data <- read.delim(glue("/mnt/sda2/TMB/Data/VEP_92_NSLC/NSLC_{Patient}.vcf"), sep = "\t", skip = VEP.header-1, header = TRUE)
+  VCF.info <- VEP.raw[c(VCF.info_lines[1:length(VCF.info_lines)-1])] %>% gsub(".*ID=", "", .) %>% gsub(",.*", "", .)
+  VEP.format <- VEP.raw[VEP.format_line] %>% gsub(".*Format: ", "", .) %>% gsub("\">", "", .) %>% str_split(., "\\|") %>% unlist()
+  VEP_data <- read.delim(glue("/mnt/sda2/TMB/Data/VEP_pick_92_NSLC/NSLC_{Patient}.vcf"), sep = "\t", skip = VEP.header-1, header = TRUE)
   # Removing MT chromosome variants
   VEP_data <- VEP_data[VEP_data$X.CHROM != "MT",]
+  # Adding patient ID to the ID field
+  VEP_data$ID <- paste0(glue("{Patient}:"), VEP_data$ID)
 }
   
 # Windows
@@ -45,14 +52,20 @@ if(Sys.info()['sysname'] == "Windows") {
   clin_data <- read.xlsx("Data/NCI_NeverSmoker_n92_20210812_TMB_drivers.xlsx", sheetIndex = 1)
   adeno_list <- clin_data$Patient_ID[clin_data$histology == "3"]
   
-  VEP.header <- grep('##', readLines(glue("Data/VEP_92_NSLC/NSLC_{Patient}.vcf")), invert = TRUE, fixed = TRUE, )[1]
-  VCF.info_lines <- grep('##INFO=', readLines(glue("Data/VEP_92_NSLC/NSLC_{Patient}.vcf")), fixed = TRUE)
+  # Getting the description and data lines from vcf files
+  VEP.raw <- readLines(glue("Data/VEP_pick_92_NSLC/NSLC_{Patient}.vcf"))
+  VEP.header <- grep('##', VEP.raw, invert = TRUE, fixed = TRUE, )[1]
+  VCF.info_lines <- grep('##INFO=', VEP.raw, fixed = TRUE)
   VEP.format_line <- last(VCF.info_lines)
-  VCF.info <- readLines(glue("Data/VEP_92_NSLC/NSLC_{Patient}.vcf"))[c(VCF.info_lines[1:length(VCF.info_lines)-1])] %>% gsub(".*ID=", "", .) %>% gsub(",.*", "", .)
-  VEP.format <- readLines(glue("Data/VEP_92_NSLC/NSLC_{Patient}.vcf"))[VEP.format_line] %>% gsub(".*Format: ", "", .) %>% gsub("\">", "", .) %>% str_split(., "\\|") %>% unlist()
-  VEP_data <- read.delim(glue("Data/VEP_92_NSLC/NSLC_{Patient}.vcf"), sep = "\t", skip = VEP.header-1, header = TRUE)
+  VCF.info <- VEP.raw[c(VCF.info_lines[1:length(VCF.info_lines)-1])] %>% gsub(".*ID=", "", .) %>% gsub(",.*", "", .)
+  VEP.format <- VEP.raw[VEP.format_line] %>% gsub(".*Format: ", "", .) %>% gsub("\">", "", .) %>% str_split(., "\\|") %>% unlist()
+  VEP_data <- read.delim(glue("Data/VEP_pick_92_NSLC/NSLC_{Patient}.vcf"), sep = "\t", skip = VEP.header-1, header = TRUE)
+  
   # Removing MT chromosome variants
   VEP_data <- VEP_data[VEP_data$X.CHROM != "MT",]
+  
+  # Adding patient ID to the ID field
+  VEP_data$ID <- paste0(glue("{Patient}:"), VEP_data$ID)
 }
 
 # Data formatting
@@ -62,21 +75,22 @@ VEP_data$EXTRA <- sapply(VEP.extra, "[[", 2)
 VEP_data$EXTRA <- lapply(VEP_data$EXTRA, gsub, pattern = 'CSQ=', replacement='')
 VEP_data.EXTRA_split_rows <- separate_rows(VEP_data, EXTRA, sep=",")
 VEP_data.EXTRA_split_cols <- separate(VEP_data.EXTRA_split_rows, EXTRA, c(VEP.format), sep = "\\|")
+
+# Data harmonizing: reporting the worst (first) consequence of variants when multiple consequences are annotated by VEP.
+# Order of consequence severity: https://grch37.ensembl.org/info/genome/variation/prediction/predicted_data.html
+VEP_data.EXTRA_split_cols$worst_consequence <- sapply(strsplit(VEP_data.EXTRA_split_cols$Consequence, "&", perl=TRUE), "[[", 1)
+VEP_data.EXTRA_split_cols <- VEP_data.EXTRA_split_cols %>% relocate(worst_consequence, .after=Consequence)
+
+VEP_data.EXTRA_split_cols$region_type <- unlist(sapply(1:nrow(VEP_data.EXTRA_split_cols), function(x) {names(region_types[grep(VEP_data.EXTRA_split_cols$worst_consequence[x], region_types)])}))
+
+unique(VEP_data.EXTRA_split_cols$worst_consequence) %in% as.vector(unlist(region_types))
+
+# Final data set for each patient
 assign(glue("VEP_data.NSLC_{Patient}"), VEP_data.EXTRA_split_cols)
 
-# Data cleaning (harmonizing variants when multiple are annotated by VEP)
-
-# Mutation types annotated by VEP are available here: https://grch37.ensembl.org/info/genome/variation/prediction/predicted_data.html
-region_types <- new.env(hash = TRUE)
-region_types[["intergenic"]] <- c("downstream_gene_variant", "upstream_gene_variant", "intergenic_variant")
-region_types[["intron"]] <- c("intron_variant", "non_coding_transcript_variant", "splice_region_variant", "splice_donor_variant", "splice_acceptor_variant")
-region_types[["regulatory"]] <- c("regulatory_region_variant", "TF_binding_site_variant")
-region_types[["exon"]] <- c("non_coding_transcript_exon_variant", "missense_variant", "synonymous_variant", "3_prime_UTR_variant", "5_prime_UTR_variant", "coding_sequence_variant", "frameshift_variant", "stop_gained")
-
-unique(unlist(strsplit(VEP_data.NSLC_0001$Consequence, "&", perl=TRUE)))
 
 ## TO DO : 
-# Vérifier si tous les types de régions sont présents.
-# Trouver comment garder un seul variant par type de régions (prendre l'annotation la plus importante?)
+# - Vérifier si tous les types de régions sont présents.
+# - Est-ce qu'on garde les synonymous variants?
 
 
