8e5f74ac3ceaa7e1f36f1f3328b7ae275a5ea30a
LJRuel <louis-jacques.ruel.1@ulaval.ca>
2023-02-13 17:37:04 -0500

    chromosome_plot

--- 2-82_NSLC_graphs.r
+++ 2-82_NSLC_graphs.r
@@ -5,11 +5,15 @@
 # - Make a similar graph that combines all the patients.
 # - Compare TMBs with SBS signatures
 
+#BiocManager::install("MutationalPatterns")
+
 library(VariantAnnotation)
+library(GenomicFeatures)
+library(AnnotationHub)
 library(tidyverse)
 library(data.table)
 library(glue)
-library(chromoMap)
+library(chromoMap) # reference: https://bmcbioinformatics.biomedcentral.com/articles/10.1186/s12859-021-04556-z
 
 setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
 
@@ -28,7 +32,6 @@ GRCh37.gtf<- ah[['AH10684']]
 
 ## subset the gtf files for only protein_coding genes and lincRNAs
 GRCh37.gtf<- GRCh37.gtf[GRCh37.gtf$gene_biotype %in% c('protein_coding', 'lincRNA')]
-table(GRCh37.gtf$gene_biotype)
 
 ## make a txdb and keep conventional chromosomes
 GRCh37.txdb <- makeTxDbFromGRanges(GRCh37.gtf)
@@ -41,15 +44,39 @@ chrom_data[, END_POS:=end(chrom_grngs)%>%as.numeric()]
 
 anno_data <- VEP_data[, list(ID, CHROM, POS, END_POS)]
 
-## TODO
-# - Déterminer les windows
-# - Compter le nb de variants par window
-# - Donner ce nombre pour le mapping
+chromo <- chromoMap(list(chrom_data), list(anno_data))
 
-chromoMap(list(chrom_data), list(anno_data),
+VEP_data[, loci:= chromo$x$chData[[1]]$loci]
+window_count <- as.data.table(table(VEP_data[, loci]))
+VEP_data <- merge(VEP_data, window_count, by.x = "loci", by.y = "V1")
+setnames(VEP_data, "N", "loci_var_count")
+VEP_data <- VEP_data %>% mutate(loci_var_cat = case_when(loci_var_count < 5 ~ "[1, 5]",
+                                                         loci_var_count >= 5 & loci_var_count <10 ~ "[5, 10[",
+                                                         loci_var_count >=10 ~ "[10, ["))
+
+anno_data_2 <- VEP_data[, list(ID, CHROM, POS, END_POS, loci_var_count, loci_var_cat)]
+
+chromoMap(list(chrom_data), list(anno_data_2),
           data_based_color_map = T,
           data_type = "numeric",
-          plots = "bar",
-          aggregate_func = "count",
-          heat_map = F)
+          plots = "scatter",
+          plot_filter = list(c("col","byCategory")),
+          ch2D.colors = c("pink3","orange3","purple"))
+
+bla <- window_count[order(window_count[, N], decreasing = TRUE),]
+median(bla$N)
+
+################################################################################
+################################### ARCHIVES ###################################
+################################################################################
+
+window_length <- chromo$x$chData[[1]]$loci_end[1] - chromo$x$chData[[1]]$loci_start[1]
+
+for(chromosome in unique(VEP_data[, CHROM])) {
+  chr_indices <- c(first(grep(glue("chromap-{chromosome}-"),  chromo$x$chData[[1]]$loci)), last(grep(glue("chromap-{chromosome}-"),  chromo$x$chData[[1]]$loci)))
+  assign(glue("chr_{chromosome}"), chr_indices)
+  assign(glue("chr_{chromosome}_wd_length"), chromo$x$chData[[1]]$loci_end[chr_indices[1]] - chromo$x$chData[[1]]$loci_start[chr_indices[1]])
+}
+
+
 
