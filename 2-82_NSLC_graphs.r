8b8ccabb3af4fbef00d623e84187f384ac6e66f2
LJRuel <louis-jacques.ruel.1@ulaval.ca>
2023-02-14 07:35:14 -0500

    Update 2-82_NSLC_graphs.r

--- 2-82_NSLC_graphs.r
+++ 2-82_NSLC_graphs.r
@@ -14,16 +14,26 @@ library(tidyverse)
 library(data.table)
 library(glue)
 library(chromoMap) # reference: https://bmcbioinformatics.biomedcentral.com/articles/10.1186/s12859-021-04556-z
+library(xlsx)
 
 setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
 
-Patient="0001"
+# Clinicopathological data - keeping only adenocarinomas
+clin_data <- as.data.table(read.xlsx("Data/NCI_NeverSmoker_n92_20210812_TMB_drivers.xlsx", sheetIndex = 1))
+clin_data <- clin_data[histology == 3, ]
+Patients_list <- gsub("NSLC-", "",sort(clin_data[, Patient_ID]))
 
-VEP_data <- fread(glue("Data/VEP_82_NSLC_TMB/VEP_NSLC-{Patient}.csv"))
+VEP_data_all_patients <- data.table()
+for(Patient in Patients_list) {
+  VEP_data <- fread(glue("Data/VEP_82_NSLC_TMB/VEP_NSLC-{Patient}.csv"))
+  VEP_data_all_patients <- rbindlist(list(VEP_data_all_patients, VEP_data), use.names = TRUE)
+}
 
-VEP_data <- VEP_data %>%  mutate(END_POS = case_when(mutation_type == "SNV" ~ POS,
-                                                     mutation_type == "insertion" ~ POS,
-                                                     mutation_type == "deletion" ~ POS+nchar(REF))) %>% relocate(END_POS, .after = POS)
+VEP_data_all_patients <- VEP_data_all_patients %>%  mutate(END_POS = case_when(mutation_type == "SNV" ~ POS,
+                                                                               mutation_type == "insertion" ~ POS,
+                                                                               mutation_type == "deletion" ~ POS+nchar(REF))) %>% relocate(END_POS, .after = POS)
+
+VEP_data_all_patients <- VEP_data_all_patients[order(VEP_data_all_patients[,CHROM])]
 
 ah = AnnotationHub()
 #AnnotationHub::query(ah, c('gtf', 'Homo_sapiens', 'GRCh37'))
@@ -42,29 +52,33 @@ chrom_data[, CHROM:=seqnames(chrom_grngs)%>%as.character()]
 chrom_data[, POS:=start(chrom_grngs)%>%as.numeric()]
 chrom_data[, END_POS:=end(chrom_grngs)%>%as.numeric()]
 
-anno_data <- VEP_data[, list(ID, CHROM, POS, END_POS)]
+anno_data <- VEP_data_all_patients[, list(ID, CHROM, POS, END_POS)]
 
 chromo <- chromoMap(list(chrom_data), list(anno_data))
 
-VEP_data[, loci:= chromo$x$chData[[1]]$loci]
-window_count <- as.data.table(table(VEP_data[, loci]))
-VEP_data <- merge(VEP_data, window_count, by.x = "loci", by.y = "V1")
-setnames(VEP_data, "N", "loci_var_count")
+VEP_data_all_patients[, loci:= chromo$x$chData[[1]]$loci]
+window_count <- as.data.table(table(VEP_data_all_patients[, loci]))
+VEP_data_all_patients <- merge(VEP_data_all_patients, window_count, by.x = "loci", by.y = "V1")
+setnames(VEP_data_all_patients, "N", "loci_var_count")
 VEP_data <- VEP_data %>% mutate(loci_var_cat = case_when(loci_var_count < 5 ~ "[1, 5]",
                                                          loci_var_count >= 5 & loci_var_count <10 ~ "[5, 10[",
                                                          loci_var_count >=10 ~ "[10, ["))
 
-anno_data_2 <- VEP_data[, list(ID, CHROM, POS, END_POS, loci_var_count, loci_var_cat)]
+anno_data_2 <- VEP_data_all_patients[, list(ID, CHROM, POS, END_POS, loci_var_count)]
 
 chromoMap(list(chrom_data), list(anno_data_2),
           data_based_color_map = T,
           data_type = "numeric",
           plots = "scatter",
           plot_filter = list(c("col","byCategory")),
-          ch2D.colors = c("pink3","orange3","purple"))
+          ch2D.colors = c("red3","orange3","purple"),
+          remove.last.window = FALSE)
 
-bla <- window_count[order(window_count[, N], decreasing = TRUE),]
-median(bla$N)
+chromoMap(list(chrom_data), list(anno_data_2),
+          data_based_color_map = T,
+          data_type = "numeric",
+          plots = "bar",
+          remove.last.window = FALSE)
 
 ################################################################################
 ################################### ARCHIVES ###################################
