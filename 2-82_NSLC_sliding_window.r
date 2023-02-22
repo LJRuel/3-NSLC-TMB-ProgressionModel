1c970bc5a3ca50b79a0feae53ad4b2a834a172e6
LJRuel <louis-jacques.ruel.1@ulaval.ca>
2023-02-22 15:56:57 -0500

    Update 2-82_NSLC_sliding_window.r

--- 2-82_NSLC_sliding_window.r
+++ 2-82_NSLC_sliding_window.r
@@ -11,4 +11,13 @@ Patient="0001"
 VEP_data <- fread(glue("Data/VEP_82_NSLC_TMB/VEP_NSLC-{Patient}.csv"))
 
 #https://www.google.com/search?q=r+genomic+sliding+window&oq=r+genomic+sliding+window&aqs=edge..69i57j0i30i546i625.11434j0j1&sourceid=chrome&ie=UTF-8
+GRanges(VEP_data)
+gr <- makeGRangesFromDataFrame(VEP_data, keep.extra.columns = TRUE, ignore.strand = TRUE, 
+                               start.field = "POS", end.field = "END_POS")
 
+VEP_data[, .(
+  window.start = rollapply(locus, width=3, by=2, FUN=min, align="left"),
+  window.end = rollapply(locus, width=3, by=2, FUN=max, align="left"),
+  coverage = rollapply(depth, width=3, by=2, FUN=mean, align="left")
+),
+.(CHROM)]
